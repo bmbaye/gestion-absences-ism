df04039977e02a34aacd965f8077c96612da2888
babacar <babacarmbaye2400@gmail.com>
2025-11-18 15:22:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gestion-absences-ism',
+  webDir: 'www'
+};
+
+export default config;
